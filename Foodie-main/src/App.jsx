89d87cc61a5082acfed6ac81a89f6795dedb4453
KavishkaSashmitha <132291481+KavishkaSashmitha@users.noreply.github.com>
2024-06-15 01:31:43 +0530

    removed Navbar Items Home  & Shop Remain

--- Foodie-main/src/App.jsx
+++ Foodie-main/src/App.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-import { BrowserRouter, Route, Routes, } from "react-router-dom";
-import "./App.css";
-import Layout from "./Layout/Layout";
-import Shop from "./components/Shop";
-import Pages from "./Layout/Pages";
-import { RecoilRoot } from "recoil";
-import Test from "./components/test";
-import Login from "./components/Login";
-import { AuthProvider, useAuth } from "./config/Context";
-import Blog from "./components/Blog";
-import Blog1 from "./components/Blog1";
-import RouteTracker from "./RouterTracker";
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Layout from './Layout/Layout';
+import Shop from './components/Shop';
+import Pages from './Layout/Pages';
+import { RecoilRoot } from 'recoil';
+import Test from './components/test';
+import Login from './components/Login';
+import { AuthProvider } from './config/Context';
+import Blog from './components/Blog';
+import Blog1 from './components/Blog1';
+import RouteTracker from './RouterTracker';
 
 function App() {
   return (
@@ -30,26 +27,11 @@ function App() {
                 }
               >
                 <Route index element={<Pages />} />
-                <Route
-                  path="shop"
-                  element={<Shop />}
-                />
-                <Route
-                  path="signup"
-                  element={<Test />}
-                />
-                <Route
-                  path="login"
-                  element={<Login />}
-                />
-                <Route
-                  path="blogs"
-                  element={<Blog />}
-                />
-                <Route
-                  path="blogs/1"
-                  element={<Blog1 />}
-                />
+                <Route path="shop" element={<Shop />} />
+                <Route path="signup" element={<Test />} />
+                <Route path="login" element={<Login />} />
+                <Route path="blogs" element={<Blog />} />
+                <Route path="blogs/1" element={<Blog1 />} />
               </Route>
             </Routes>
             <RouteTracker />
@@ -61,5 +43,3 @@ function App() {
 }
 
 export default App;
-
-
