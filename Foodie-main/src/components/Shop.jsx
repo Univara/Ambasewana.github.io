afa6ae06df9207e44975cbd4332c51570ef7e059
Pasindu Rasanjana <pasindurasanjana457825@gmail.com>
2024-06-16 01:13:22 +0530

    position

--- Foodie-main/src/components/Shop.jsx
+++ Foodie-main/src/components/Shop.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
-import { atom, useRecoilState, selector } from 'recoil';
-import { BsFillCartPlusFill, BsCartCheckFill } from 'react-icons/bs';
-import Transition from '../components/Transition';
-import Notification from '../components/notification';
-import './Styles/shop.css';
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import { atom, useRecoilState, selector } from "recoil";
+import { BsFillCartPlusFill, BsCartCheckFill } from "react-icons/bs";
+import Transition from "../components/Transition";
+import Notification from "../components/notification";
+import "./Styles/shop.css";
 
 // Define Recoil state atoms and selectors
 export const CartState = atom({
-  key: 'CartState',
+  key: "CartState",
   default: {},
 });
 
 export const cartStateWithRemove = selector({
-  key: 'cartStateWithRemove',
+  key: "cartStateWithRemove",
   get: ({ get }) => get(CartState),
   set: ({ set }, updatedCart) => {
     set(CartState, updatedCart);
-    localStorage.setItem('cart', JSON.stringify(updatedCart)); // Save to localStorage
+    localStorage.setItem("cart", JSON.stringify(updatedCart)); // Save to localStorage
   },
 });
 
@@ -39,13 +39,13 @@ function Shop() {
   const [visible, setVisible] = useState(10); // Controls the number of visible items
   const [searchParams] = useSearchParams();
   const [items, setItems] = useState([]); // State for the items
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [showTransition, setShowTransition] = useState(true);
   const [notification, setNotification] = useState({
-    message: '',
+    message: "",
     visible: false,
   });
-  const [foodCategory, setFoodCategory] = useState('chinese'); // Default category is Chinese
+  const [foodCategory, setFoodCategory] = useState("chinese"); // Default category is Chinese
 
   useEffect(() => {
     // Fetch data based on foodCategory
@@ -55,12 +55,12 @@ function Shop() {
           `http://localhost:3000/api/products/${foodCategory}`
         );
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const data = await response.json();
         setItems(data); // Set the items state with the fetched data
       } catch (error) {
-        console.error('Fetch error:', error);
+        console.error("Fetch error:", error);
       }
     };
 
@@ -78,7 +78,7 @@ function Shop() {
   }, []);
 
   // Filter and search logic
-  const typeFilter = searchParams.get('category');
+  const typeFilter = searchParams.get("category");
   const displayedItems = items.filter((item) => {
     const matchesType = typeFilter ? item.category === typeFilter : true;
     const matchesSearch = item.name
@@ -114,7 +114,7 @@ function Shop() {
       <div className="item-info">
         <h2 className="name">{item.name}</h2>
         <p className="price">
-          Rs.{item.price}{' '}
+          Rs.{item.price}{" "}
           <span className="original-price">{item.originalPrice}</span>
         </p>
         <button
@@ -147,8 +147,32 @@ function Shop() {
 
   // Map food categories to their respective filter options
   const filterOptions = {
-    chinese: ['Soup', 'Quick Dishes', 'Rice', 'Noodles', 'Pasta', 'Chopsuey(Rice)', 'Chopsuey(No Rice)', 'Devilled', 'Curry', 'Prawns', 'Fish', 'Chicken', 'Cuttle Fish'],
-    indian: ['Salad', 'Vegetable', ' Non Vegetable', 'Vegetable Pulow', 'Chicken Kebab', 'Vegetable Kebab', 'Roti Naan', 'Set Menu', 'Kottu Naan'],
+    chinese: [
+      "Soup",
+      "Quick Dishes",
+      "Rice",
+      "Noodles",
+      "Pasta",
+      "Chopsuey(Rice)",
+      "Chopsuey(No Rice)",
+      "Devilled",
+      "Curry",
+      "Prawns",
+      "Fish",
+      "Chicken",
+      "Cuttle Fish",
+    ],
+    indian: [
+      "Salad",
+      "Vegetable",
+      " Non Vegetable",
+      "Vegetable Pulow",
+      "Chicken Kebab",
+      "Vegetable Kebab",
+      "Roti Naan",
+      "Set Menu",
+      "Kottu Naan",
+    ],
   };
 
   return (
@@ -158,29 +182,36 @@ function Shop() {
         message={notification.message}
         visible={notification.visible}
       />
-      <h1>Explore Our Items</h1>
 
-      {/* Food category switch */}
-      <div className="food-category-switch switch-container">
-        <div className="switch">
-          <input
-            type="radio"
-            id="chinese"
-            value="chinese"
-            checked={foodCategory === 'chinese'}
-            onChange={() => setFoodCategory('chinese')}
-          />
-          <label htmlFor="chinese" className="switch-label">Chinese</label>
-        </div>
-        <div className="switch">
-          <input
-            type="radio"
-            id="indian"
-            value="indian"
-            checked={foodCategory === 'indian'}
-            onChange={() => setFoodCategory('indian')}
-          />
-          <label htmlFor="indian" className="switch-label">Indian</label>
+      <div className="header-container">
+        <h1>Explore Our Items</h1>
+
+        {/* Food category switch */}
+        <div className="food-category-switch switch-container">
+          <div className="switch">
+            <input
+              type="radio"
+              id="chinese"
+              value="chinese"
+              checked={foodCategory === "chinese"}
+              onChange={() => setFoodCategory("chinese")}
+            />
+            <label htmlFor="chinese" className="switch-label">
+              Chinese
+            </label>
+          </div>
+          <div className="switch">
+            <input
+              type="radio"
+              id="indian"
+              value="indian"
+              checked={foodCategory === "indian"}
+              onChange={() => setFoodCategory("indian")}
+            />
+            <label htmlFor="indian" className="switch-label">
+              Indian
+            </label>
+          </div>
         </div>
       </div>
 
@@ -198,13 +229,13 @@ function Shop() {
       </div>
 
       <nav className="filter-nav">
-        <Link className={`item-type ${!typeFilter ? 'selected' : ''}`} to=".">
+        <Link className={`item-type ${!typeFilter ? "selected" : ""}`} to=".">
           All
         </Link>
         {filterOptions[foodCategory].map((category) => (
           <Link
             key={category}
-            className={`item-type ${typeFilter === category ? 'selected' : ''}`}
+            className={`item-type ${typeFilter === category ? "selected" : ""}`}
             to={`?category=${category}`}
           >
             {category}
