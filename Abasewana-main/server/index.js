01ecd74328f794cc7f845e49608f53c0696d2f53
Pasindu Rasanjana <pasindurasanjana457825@gmail.com>
2024-06-17 19:11:04 +0530

    error fix

--- Abasewana-main/server/index.js
+++ Abasewana-main/server/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const admin = require('firebase-admin');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const serviceAccount = require('./key.json');
-const WebSocket = require('ws');
+const express = require("express");
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const serviceAccount = require("./key.json");
+const WebSocket = require("ws");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL:
-    'https://ambasewana-a6fa5-default-rtdb.asia-southeast1.firebasedatabase.app',
-  storageBucket: 'gs://ambasewana-a6fa5.appspot.com',
+    "https://ambasewana-a6fa5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  storageBucket: "gs://ambasewana-a6fa5.appspot.com",
 });
 
 const db = admin.firestore();
@@ -25,15 +25,15 @@ app.use(fileUpload());
 // WebSocket server setup
 const wss = new WebSocket.Server({ noServer: true });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
+wss.on("connection", (ws) => {
+  console.log("Client connected");
 
-  ws.on('message', (message) => {
+  ws.on("message", (message) => {
     console.log(`Received message => ${message}`);
   });
 
-  ws.on('close', () => {
-    console.log('Client disconnected');
+  ws.on("close", () => {
+    console.log("Client disconnected");
   });
 });
 
@@ -51,21 +51,21 @@ const server = app.listen(port, () => {
 });
 
 // Upgrade HTTP server to handle WebSocket connections
-server.on('upgrade', (request, socket, head) => {
+server.on("upgrade", (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
-    wss.emit('connection', ws, request);
+    wss.emit("connection", ws, request);
   });
 });
 
 // API endpoints
 
 // Endpoint to add Indian products
-app.post('/api/products/indian', async (req, res) => {
+app.post("/api/products/indian", async (req, res) => {
   try {
     const { name, description, price, category } = req.body;
 
     if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).json({ error: 'No image file provided' });
+      return res.status(400).json({ error: "No image file provided" });
     }
 
     const imageFile = req.files.image;
@@ -79,11 +79,11 @@ app.post('/api/products/indian', async (req, res) => {
     });
 
     const imageUrl = await storageRef.getSignedUrl({
-      action: 'read',
-      expires: '03-01-2500',
+      action: "read",
+      expires: "03-01-2500",
     });
 
-    const docRef = await db.collection('indian_products').add({
+    const docRef = await db.collection("indian_products").add({
       name,
       description,
       price,
@@ -93,18 +93,18 @@ app.post('/api/products/indian', async (req, res) => {
 
     res.status(201).json({ id: docRef.id });
   } catch (error) {
-    console.error('Error adding product: ', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error adding product: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Endpoint to add Chinese products
-app.post('/api/products/chinese', async (req, res) => {
+app.post("/api/products/chinese", async (req, res) => {
   try {
     const { name, description, price, category } = req.body;
 
     if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).json({ error: 'No image file provided' });
+      return res.status(400).json({ error: "No image file provided" });
     }
 
     const imageFile = req.files.image;
@@ -118,11 +118,11 @@ app.post('/api/products/chinese', async (req, res) => {
     });
 
     const imageUrl = await storageRef.getSignedUrl({
-      action: 'read',
-      expires: '03-01-2500',
+      action: "read",
+      expires: "03-01-2500",
     });
 
-    const docRef = await db.collection('chinese_products').add({
+    const docRef = await db.collection("chinese_products").add({
       name,
       description,
       price,
@@ -132,15 +132,15 @@ app.post('/api/products/chinese', async (req, res) => {
 
     res.status(201).json({ id: docRef.id });
   } catch (error) {
-    console.error('Error adding product: ', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error adding product: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Endpoint to fetch all Indian products
-app.get('/api/products/indian', async (req, res) => {
+app.get("/api/products/indian", async (req, res) => {
   try {
-    const snapshot = await db.collection('indian_products').get();
+    const snapshot = await db.collection("indian_products").get();
     const products = [];
 
     snapshot.forEach((doc) => {
@@ -152,15 +152,15 @@ app.get('/api/products/indian', async (req, res) => {
 
     res.json(products);
   } catch (error) {
-    console.error('Error getting products: ', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error getting products: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Endpoint to fetch all Chinese products
-app.get('/api/products/chinese', async (req, res) => {
+app.get("/api/products/chinese", async (req, res) => {
   try {
-    const snapshot = await db.collection('chinese_products').get();
+    const snapshot = await db.collection("chinese_products").get();
     const products = [];
 
     snapshot.forEach((doc) => {
@@ -172,15 +172,15 @@ app.get('/api/products/chinese', async (req, res) => {
 
     res.json(products);
   } catch (error) {
-    console.error('Error getting products: ', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error getting products: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Endpoint to fetch main products (example: limiting to 2 for demonstration)
-app.get('/api/products/main', async (req, res) => {
+app.get("/api/products/main", async (req, res) => {
   try {
-    const snapshot = await db.collection('products').limit(2).get();
+    const snapshot = await db.collection("products").limit(2).get();
     const products = [];
 
     snapshot.forEach((doc) => {
@@ -192,24 +192,24 @@ app.get('/api/products/main', async (req, res) => {
 
     res.json(products);
   } catch (error) {
-    console.error('Error getting products: ', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error getting products: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Endpoint to fetch a product by ID
-app.get('/api/products/:id', async (req, res) => {
+app.get("/api/products/:id", async (req, res) => {
   try {
     const productId = req.params.id;
 
     if (!productId) {
-      return res.status(400).json({ error: 'Product ID is required' });
+      return res.status(400).json({ error: "Product ID is required" });
     }
 
-    const docRef = await db.collection('products').doc(productId).get();
+    const docRef = await db.collection("products").doc(productId).get();
 
     if (!docRef.exists) {
-      return res.status(404).json({ error: 'Product not found' });
+      return res.status(404).json({ error: "Product not found" });
     }
 
     const productData = docRef.data();
@@ -219,20 +219,20 @@ app.get('/api/products/:id', async (req, res) => {
       ...productData,
     });
   } catch (error) {
-    console.error('Error getting product by ID: ', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error getting product by ID: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Endpoint to add an order
-app.post('/api/orders', async (req, res) => {
+app.post("/api/orders", async (req, res) => {
   try {
     const { customerName, table, items, orderNumber, orderStatus } = req.body;
 
     if (!customerName || !orderNumber || !orderStatus) {
       return res.status(400).json({
         error:
-          'Please provide all required fields: customerName, table, orderNumber, orderStatus, and valid items array with itemName, image, price, and quantity',
+          "Please provide all required fields: customerName, table, orderNumber, orderStatus, and valid items array with itemName, image, price, and quantity",
       });
     }
 
@@ -250,20 +250,20 @@ app.post('/api/orders', async (req, res) => {
       dateTime: new Date().toISOString(),
     };
 
-    const orderRef = await db.collection('orders').add(orderData);
+    const orderRef = await db.collection("orders").add(orderData);
 
     // Broadcast the new order to all connected clients
     broadcast(orderData);
 
     res.status(201).json({ orderId: orderRef.id });
   } catch (error) {
-    console.error('Error adding order: ', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error adding order: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Endpoint to update order status
-app.put('/api/orders/:id', async (req, res) => {
+app.put("/api/orders/:id", async (req, res) => {
   try {
     const orderId = req.params.id;
     const { orderStatus } = req.body;
@@ -271,14 +271,14 @@ app.put('/api/orders/:id', async (req, res) => {
     if (!orderId || !orderStatus) {
       return res
         .status(400)
-        .json({ error: 'Order ID and orderStatus are required' });
+        .json({ error: "Order ID and orderStatus are required" });
     }
 
-    const orderRef = db.collection('orders').doc(orderId);
+    const orderRef = db.collection("orders").doc(orderId);
     const snapshot = await orderRef.get();
 
     if (!snapshot.exists) {
-      return res.status(404).json({ error: 'Order not found' });
+      return res.status(404).json({ error: "Order not found" });
     }
 
     await orderRef.update({ orderStatus });
@@ -292,14 +292,9 @@ app.put('/api/orders/:id', async (req, res) => {
     // Broadcast updated order to all connected clients
     broadcast(updatedOrderData);
 
-    res.status(200).json({ message: 'Order status updated successfully' });
+    res.status(200).json({ message: "Order status updated successfully" });
   } catch (error) {
-    console.error('Error updating order status: ', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error updating order status: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
